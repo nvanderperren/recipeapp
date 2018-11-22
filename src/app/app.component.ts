41866c6c3ed1ca291e2b34e2d31d508ae76ff483
nvanderperren <ik.ben.nas@gmail.com>
2018-11-22 15:37:54 +0100

    add async filter method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Recipe } from './recipe/recipe.model';
 import { RecipeDataService } from './recipe-data.service';
+import { Subject } from 'rxjs';
+import { distinctUntilChanged, debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,20 @@ export class AppComponent {
 
   private _recipes: Recipe[];
   public filterRecipeName: string;
+  public filterRecipe$ = new Subject<string>();
 
   constructor(private _recipeDataService: RecipeDataService) {
     this._recipes = this._recipeDataService.recipes;
+    this.filterRecipe$
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(400),
+        map(val => val.toLowerCase())
+
+      )
+      .subscribe(
+      val => this.filterRecipeName = val
+    );
   }
 
   get recipes() {

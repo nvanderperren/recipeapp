8d5c44ddf8e16a949a207b375f04e95929911719
nvanderperren <ik.ben.nas@gmail.com>
2018-11-22 16:27:25 +0100

    add connection to database

---
+++ backend/models/Recipe.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var RecipeSchema = new mongoose.Schema({
+    name: String,
+    ingredients: [String],
+    created: Date
+
+})
+
+mongoose.model('Recipe', RecipeSchema)
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
+let Recipe = mongoose.model('Recipe');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('server works');
+});
+
+router.get('/API/recipes/', function (req, res, next) {
+    Recipe.find(function (err, recipes) {
+        if (err) { return next(err); }
+        res.json(recipes);
+    });
 });
 
 module.exports = router;

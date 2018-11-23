5655d2116dd8dd1520c75c2c71afd9fd74043340
nvanderperren <ik.ben.nas@gmail.com>
2018-11-23 18:18:09 +0100

    add minimalistic version of CORS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './recipe/recipe.component';
@@ -16,7 +17,8 @@ import { RecipeFilterPipe } from './recipe-filter.pipe';
     RecipeFilterPipe
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
